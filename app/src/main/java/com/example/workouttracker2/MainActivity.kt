257084d6b0902a8da3de7c5049b685770eadaa84
Nikolaus.Stift <nstift.work@gmail.com>
2024-02-06 14:01:41 +0100

    Bottom Nav bar Manifest caerma use (no CameraX yet) Dependency for ML Kit

--- app/src/main/java/com/example/workouttracker2/MainActivity.kt
+++ app/src/main/java/com/example/workouttracker2/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.workouttracker2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.workouttracker2.repository.ExerciseDao
 import com.example.workouttracker2.repository.SetDao
 import com.example.workouttracker2.repository.WorkoutDao
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 lateinit var exerciseDao: ExerciseDao
@@ -20,5 +23,11 @@ class MainActivity : AppCompatActivity() {
         exerciseDao = myAppApplication.exerciseDao
         setDao = myAppApplication.setDao
         workoutDao = myAppApplication.workoutDao
+
+
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/workouttracker2/NutritionOverview/NutritionOverviewFragment.kt
@@ -0,0 +1,8 @@
+package com.example.workouttracker2.NutritionOverview
+
+import androidx.fragment.app.Fragment
+import com.example.workouttracker2.R
+
+class NutritionOverviewFragment : Fragment(R.layout.fragment_nutrition_overview) {
+    // Implementiere Logik ähnlich wie bei WorkoutListFragment
+}
