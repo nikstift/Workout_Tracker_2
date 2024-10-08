519a0d115e5cee0f2757a8f08bd0fe9ee789ac25
nikst <nstift.work@gmail.com>
2024-10-08 15:00:09 +0200

    Adjustments NutritionOverviewFragment.kt

--- app/src/main/java/com/example/workouttracker2/NutritionOverview/NutritionOverviewFragment.kt
+++ app/src/main/java/com/example/workouttracker2/NutritionOverview/NutritionOverviewFragment.kt
@@ -5,17 +5,24 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.workouttracker2.MyAppApplication
 import com.example.workouttracker2.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.mlkit.vision.barcode.common.Barcode
 
 class NutritionOverviewFragment : Fragment(R.layout.fragment_nutrition_overview),CameraPreviewDialogFragment.BarcodeListener {
 
     private lateinit var barcodeTextView: TextView
+    private lateinit var firestoreDb: FirebaseFirestore
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val btnScan: Button = view.findViewById(R.id.btnScan)
         barcodeTextView = view.findViewById(R.id.barcodeTextView)
+
+        //firestore-DB von Appliaktion klasse ziehen
+        firestoreDb = (requireActivity().application as MyAppApplication).firestoreDb
 
         btnScan.setOnClickListener {
             showCameraPreviewDialog()
@@ -29,6 +36,15 @@ class NutritionOverviewFragment : Fragment(R.layout.fragment_nutrition_overview)
     override fun onBarcodeDetected(barcode: Barcode) {
         // Hier den Barcode-Text in das entsprechende Feld setzen
         barcodeTextView.text = barcode.rawValue
+        val scannedBarcode = barcode.rawValue
+        if (scannedBarcode!=null){
+            showAddFoodDialog(scannedBarcode)
+        }
+    }
+
+    private fun showAddFoodDialog(scannedBarcode: String) {
+        val builder = MaterialAlertDialogBuilder(requireContext())
+
     }
 }
 
