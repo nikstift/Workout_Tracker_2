039151bf9acaf6ef7056075070021f72923f73c7
nikst <nstift.work@gmail.com>
2024-10-10 18:54:04 +0200

    Manual positive button handeling so validation doesnt close dialog

--- app/src/main/java/com/example/workouttracker2/NutritionOverview/NutritionOverviewFragment.kt
+++ app/src/main/java/com/example/workouttracker2/NutritionOverview/NutritionOverviewFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.workouttracker2.MyAppApplication
 import com.example.workouttracker2.R
@@ -48,13 +49,14 @@ class NutritionOverviewFragment : Fragment(R.layout.fragment_nutrition_overview)
 
     private fun showAddFoodDialog(scannedBarcode: String) {
         val builder = MaterialAlertDialogBuilder(requireContext())
-        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.add_food_dialog,null)
+        val dialogView =
+            LayoutInflater.from(requireContext()).inflate(R.layout.add_food_dialog, null)
         builder.setView(dialogView)
         builder.setTitle("Lebensmittel hinzufügen")
 
         // barcode setzen
         val tvBarcode = dialogView.findViewById<TextView>(R.id.tvBarcode)
-        tvBarcode.text= "Barcode: $scannedBarcode"
+        tvBarcode.text = "Barcode: $scannedBarcode"
 
         // ref Eingabefelder
         val etName = dialogView.findViewById<EditText>(R.id.etName)
@@ -64,73 +66,114 @@ class NutritionOverviewFragment : Fragment(R.layout.fragment_nutrition_overview)
         val etKohlenhydrate = dialogView.findViewById<EditText>(R.id.etKohlenhydrate)
         val etServingSize = dialogView.findViewById<EditText>(R.id.etServingSize)
         val etUnit = dialogView.findViewById<EditText>(R.id.etUnit)
-
-        builder.setPositiveButton("Speichern"){_,_ ->
-
-            val name = etName.text.toString().trim()
-            val kalorien = etKalorien.text.toString().toDoubleOrNull()
-            val protein = etProtein.text.toString().toDoubleOrNull()
-            val fett = etFett.text.toString().toDoubleOrNull()
-            val kohlenhydrate = etKohlenhydrate.text.toString().toDoubleOrNull()
-            val servingSize = etServingSize.text.toString().toDoubleOrNull()
-            val unit = etUnit.text.toString().trim()
-
-            //Validierung:
-            if (name.isEmpty()) {
-                Toast.makeText(requireContext(), "Bitte geben Sie einen Namen ein", Toast.LENGTH_SHORT).show()
-                return@setPositiveButton
-            }
-            if (kalorien == null) {
-                Toast.makeText(requireContext(), "Bitte geben Sie eine gültige Kalorienzahl an", Toast.LENGTH_SHORT).show()
-                return@setPositiveButton
-            }
-            if (protein == null) {
-                Toast.makeText(requireContext(), "Bitte geben Sie eine gültige Proteinzahl an", Toast.LENGTH_SHORT).show()
-                return@setPositiveButton
-            }
-            if (fett == null) {
-                Toast.makeText(requireContext(), "Bitte geben Sie eine gültige Fettzahl an", Toast.LENGTH_SHORT).show()
-                return@setPositiveButton
-            }
-            if (kohlenhydrate == null) {
-                Toast.makeText(requireContext(), "Bitte geben Sie eine gültige Kohlenhydratzahl an", Toast.LENGTH_SHORT).show()
-                return@setPositiveButton
-            }
-            if (servingSize == null) {
-                Toast.makeText(requireContext(), "Bitte geben Sie eine gültige Portionsgröße an", Toast.LENGTH_SHORT).show()
-                return@setPositiveButton
-            }
-            if (unit.isEmpty()) {
-                Toast.makeText(requireContext(), "Bitte geben Sie eine Einheit an", Toast.LENGTH_SHORT).show()
-                return@setPositiveButton
-            }
-
-            val foodData = hashMapOf(
-                "name" to name,
-                "calories" to kalorien,
-                "protein" to protein,
-                "fat" to fett,
-                "carbs" to kohlenhydrate,
-                "servingSize" to servingSize,
-                "unit" to unit
-            )
-
-
-            firestoreDb.collection("lebensmittel")
-                .document(scannedBarcode)
-                .set(foodData)
-                .addOnSuccessListener {
-                    Toast.makeText(requireContext(), "Lebensmittel erfolgreich gespeichert", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener { e ->
-                    Log.e("NutritionOverview", "Fehler beim Speichern des Lebensmittels", e)
-                    Toast.makeText(requireContext(), "Fehler beim Speichern", Toast.LENGTH_SHORT).show()
-                }
-        }
         builder.setNegativeButton("Abbrechen") { dialog, _ ->
             dialog.dismiss()
         }
-        builder.create().show()
+        builder.setPositiveButton("Speichern",null)
+        val dialog = builder.create()
+        dialog.setOnShowListener {
+            val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+
+
+            positiveButton.setOnClickListener {
+                val name = etName.text.toString().trim()
+                val kalorien = etKalorien.text.toString().toDoubleOrNull()
+                val protein = etProtein.text.toString().toDoubleOrNull()
+                val fett = etFett.text.toString().toDoubleOrNull()
+                val kohlenhydrate = etKohlenhydrate.text.toString().toDoubleOrNull()
+                val servingSize = etServingSize.text.toString().toDoubleOrNull()
+                val unit = etUnit.text.toString().trim()
+
+                //Validierung:
+                if (name.isEmpty()) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Bitte geben Sie einen Namen ein",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                if (kalorien == null) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Bitte geben Sie eine gültige Kalorienzahl an",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                if (protein == null) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Bitte geben Sie eine gültige Proteinzahl an",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                if (fett == null) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Bitte geben Sie eine gültige Fettzahl an",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                if (kohlenhydrate == null) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Bitte geben Sie eine gültige Kohlenhydratzahl an",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                if (servingSize == null) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Bitte geben Sie eine gültige Portionsgröße an",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                if (unit.isEmpty()) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Bitte geben Sie eine Einheit an",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                val foodData = hashMapOf(
+                    "name" to name,
+                    "calories" to kalorien,
+                    "protein" to protein,
+                    "fat" to fett,
+                    "carbs" to kohlenhydrate,
+                    "servingSize" to servingSize,
+                    "unit" to unit
+                )
+
+                firestoreDb.collection("lebensmittel")
+                    .document(scannedBarcode)
+                    .set(foodData)
+                    .addOnSuccessListener {
+                        Toast.makeText(
+                            requireContext(),
+                            "Lebensmittel erfolgreich gespeichert",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        dialog.dismiss()
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("NutritionOverview", "Fehler beim Speichern des Lebensmittels", e)
+                        Toast.makeText(
+                            requireContext(),
+                            "Fehler beim Speichern",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            }
+        }
+        dialog.show()
     }
 }
 
